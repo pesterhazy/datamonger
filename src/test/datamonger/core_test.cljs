9c332bac5226054756a7d9fbd5c2bcbec0c1e50b
Paulus Esterhazy <pesterhazy@gmail.com>
2020-10-17 20:28:14 +0200

    WIP

--- src/test/datamonger/core_test.cljs
+++ src/test/datamonger/core_test.cljs
@@ -1,5 +1,12 @@
 (ns datamonger.core-test
-  (:require [clojure.test :as t]))
+  (:require [clojure.test :as t]
+            [datamonger.core :as x]))
+
+(defn roundtrip [s]
+  (t/is (= s (-> s (x/hash->opts) (x/opts->hash)))))
 
 (t/deftest t1
-  (t/is (= 0 1)))
+  (roundtrip "#")
+  (roundtrip "#foo")
+  (roundtrip "#foo?a=b&c=d")
+  (t/is (= {:path "foo", :params {:a "b", :c "d"}} (x/hash->opts "#foo?a=b&c=d"))))
