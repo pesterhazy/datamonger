ea1230da70eab9f5554d99b448533f0a63f9b90e
Paulus Esterhazy <pesterhazy@gmail.com>
2020-10-17 18:12:54 +0200

    WIP

--- src/main/datamonger/core.cljs
+++ src/main/datamonger/core.cljs
@@ -1,13 +1,25 @@
 (ns datamonger.core
   (:require [clojure.core]
+            [datafrisk.core :as d]
             [reagent.core :as r]
             [reagent.dom :as rd]
             ["react" :as react]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn view-ui [v]
+  (d/DataFriskView v)
+  #_(binding [clojure.core/*print-length* 3]
+      [:div (pr-str v)]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn load+ []
   (-> (js/fetch "/examples/countries.json")
       (.then (fn [r]
-               (.json r)))))
+               (.json r)))
+      (.then (fn [r]
+               (js->clj r)))))
 
 (defn main-ui []
   (let [[v update-v] (react/useState nil)]
@@ -19,9 +31,8 @@
        js/undefined)
      #js[])
     (if v
-      (binding [clojure.core/*print-length* 3]
-        [:div (pr-str v)])
-      [:div "loading..."])))
+      [view-ui v]
+      [:div])))
 
 (def functional-compiler (r/create-compiler {:function-components true}))
 
