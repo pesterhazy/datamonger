06b0a42359fa785f9ad5236ee215e8c1f399026c
Paulus Esterhazy <pesterhazy@gmail.com>
2020-10-17 21:11:07 +0200

    WIP

--- src/test/datamonger/core_test.cljs
+++ src/test/datamonger/core_test.cljs
@@ -2,11 +2,13 @@
   (:require [clojure.test :as t]
             [datamonger.core :as x]))
 
+;; FIXME: reloading error
+
 (defn roundtrip [s]
-  (t/is (= s (-> s (x/hash->opts) (x/opts->hash)))))
+  (t/is (= s (-> s (x/url->opts) (x/opts->url)))))
 
 (t/deftest t1
-  (roundtrip "#")
-  (roundtrip "#foo")
-  (roundtrip "#foo?a=b&c=d")
-  (t/is (= {:path "foo", :params {:a "b", :c "d"}} (x/hash->opts "#foo?a=b&c=d"))))
+  (roundtrip "/")
+  (roundtrip "/foo")
+  (roundtrip "/foo?a=b&c=d")
+  (t/is (= {:pathname "foo", :params {:a "b", :c "d"}} (x/url->opts "foo?a=b&c=d"))))
