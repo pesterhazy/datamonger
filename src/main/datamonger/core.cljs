7880ed1da148c61bc39570f697d0e585fd1661cd
Paulus Esterhazy <pesterhazy@gmail.com>
2020-10-17 14:45:50 +0200

    WIP

--- src/main/datamonger/core.cljs
+++ src/main/datamonger/core.cljs
@@ -1,3 +1,11 @@
 (ns datamonger.core)
 
+(defn load+ []
+  (-> (js/fetch "/examples/countries.json")
+      (.then (fn [r]
+               (.json r)))))
+
 (prn ::hello)
+(-> (load+)
+    (.then (fn [v]
+             (js/console.log v))))
