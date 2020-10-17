d58c51303be89c58d2728be80fb8e957ed4b4c72
Paulus Esterhazy <pesterhazy@gmail.com>
2020-10-17 17:54:33 +0200

    WIP

--- src/main/datamonger/core.cljs
+++ src/main/datamonger/core.cljs
@@ -1,9 +1,16 @@
-(ns datamonger.core)
+(ns datamonger.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rd]))
 
 (defn load+ []
   (-> (js/fetch "/examples/countries.json")
       (.then (fn [r]
                (.json r)))))
+
+(defn main-ui []
+  [:h1 "hello"])
+
+(rd/render [main-ui] (js/document.getElementById "app"))
 
 (prn ::hello)
 (-> (load+)
